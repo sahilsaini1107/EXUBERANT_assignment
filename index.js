81d19f96ec18096f79644b3702072818bb04c7f4
Sahil Saini <76941329+sahilsaini1107@users.noreply.github.com>
2024-03-02 21:37:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,60 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
 const app = express();
 app.use(express.json());
-app.use(bodyParser.json());
 
-let questionnaires = {};
-let questionIdCounter = 1;
+let questionnaires = [];
 
-// Create a questionnaire
-app.post('/questionnaire', (req, res) => {
-    const data = req.body;
-    // const questionnaireId = Object.keys(questionnaires).length + 1;
-    questionnaires.push(data);
-    res.status(201).json({ message: 'Questionnaire created successfully', id: questionnaireId });
+// create a questionnaire
+app.post("/questionnaire", (req, res) => {
+  const questionnaireId = Object.keys(questionnaires).length + 1;
+  const data = req.body;
+  questionnaires[questionnaireId] = {
+    questions: data.questions || [],
+    adminAns: data.adminAns || [],
+    userAns: [],
+  };
+  res.status(201).json({
+    message: "Questionnaire created successfully",
+    id: questionnaireId,
+  });
+});
+
+// User Submit answers to questions
+app.post("/questionnaire/:questionnaireId/answers", (req, res) => {
+  const questionnaireId = parseInt(req.params.questionnaireId);
+  const data = req.body;
+  questionnaires[questionnaireId].userAns.push(data.ans);
+  res.status(200).json({ message: "Answers submitted successfully" });
 });
 
 // Retrieve questions for a questionnaire with pagination
-app.get('/questionnaire/:questionnaireId/questions', (req, res) => {
-    const questionnaireId = parseInt(req.params.questionnaireId);
-    const page = parseInt(req.query.page) || 1;
-    const perPage = 10;
-    const questionnaire = questionnaires[questionnaireId];
-    if (questionnaire) {
-        const questions = questionnaire.questions;
-        const start = (page - 1) * perPage;
-        const end = start + perPage;
-        res.json(questions.slice(start, end));
-    } else {
-        res.status(404).json({ message: 'Questionnaire not found' });
-    }
+app.get("/questionnaire/:questionnaireId/questions", (req, res) => {
+  const questionnaireId = parseInt(req.params.questionnaireId);
+  const page = parseInt(req.query.page) || 1;
+  const perPage = 10;
+  const questionnaire = questionnaires[questionnaireId];
+  if (questionnaire) {
+    const questions = questionnaire.questions;
+    // console.log(questions);
+    const start = (page - 1) * perPage;
+    const end = start + perPage;
+    res.json(questions.slice(start, end));
+  } else {
+    res.status(404).json({ message: "Questionnaire not found" });
+  }
 });
 
-// Submit answers to questions
-app.post('/questionnaire/:questionnaireId/answers', (req, res) => {
-    const questionnaireId = parseInt(req.params.questionnaireId);
-    const data = req.body;
-    const answers = data.answers;
-    res.status(200).json({ message: 'Answers submitted successfully' });
+// Retrieve users answers for a questionnaire
+app.get("/questionnaire/:questionnaireId/answers", (req, res) => {
+  const questionnaireId = parseInt(req.params.questionnaireId);
+  if (questionnaires[questionnaireId]) {
+    const answers = questionnaires[questionnaireId].userAns;
+    res.status(200).json({ answers });
+  } else {
+    res.status(404).json({ message: "Questionnaire not found" });
+  }
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
